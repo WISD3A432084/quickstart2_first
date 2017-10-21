a293235089607cf2a47de92f64a99a9a21f6c580
Xin <lovemarks19@gmail.com>
2017-10-21 22:38:15 +0800

    關聯 Task 及 User 模型

--- app/Task.php
+++ app/Task.php
@@ -12,4 +12,12 @@ class Task extends Model
      * @var array
      */
     protected $fillable = ['name'];
+
+    /**
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
