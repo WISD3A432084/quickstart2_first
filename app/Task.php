bd78a16da433049442179061593d39e3040f0c5a
Xin <lovemarks19@gmail.com>
2017-10-21 22:25:26 +0800

    宣告模型的 name 屬性應該能被「批量賦值」

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * 這些屬性能被批量賦值。
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
 }
