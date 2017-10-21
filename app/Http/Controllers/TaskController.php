b2009b16dbefadb93bd5b94c3c36bb7d06d9e58b
Xin <lovemarks19@gmail.com>
2017-10-21 22:59:22 +0800

    處理任務路由只讓已認證的使用者存取

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
